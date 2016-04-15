1023c4dc11e8e6f1aa3ecabf0faff868d4508b6f
Blaine Forbush <forbushbl@gmail.com>
2016-04-14 21:33:43 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var config = require('./.app_config');
+var mysql = require('mysql');
+var express = require('express');
+
+var pool = mysql.createPool({
+	host : config.mysql.host,
+	user: config.mysql.user,
+	password: config.mysql.password,
+	database: config.mysql.database,
+	connectionLimit: config.mysql.connectionLimit
+});
+
+var app = express();
+
+var requestCount = 0;
+app.get("/", function(req, resp) {
+	console.log('Handling request...\t' + ++requestCount);
+
+	pool.query('SELECT * FROM family', function(err, rows, fields) {
+		resp.json(rows);
+	});
+
+});
+
+console.log("Listening on port 3000");
+app.listen(3000);
+
